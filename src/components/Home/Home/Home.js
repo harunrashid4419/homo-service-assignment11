de5e017d853937be845e880f3d0c88e7716b7fbe
harunorrashid4419 <harunrashid4419@gmail.com>
2022-11-08 00:57:58 +0600

    home page service add

--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -2,19 +2,24 @@ import React from "react";
 import "./Home.css";
 import { Link, useLoaderData } from "react-router-dom";
 import Service from "../../Others/Service/Service";
+import Slider from "../Slider/Slider";
 
 const Home = () => {
    const services = useLoaderData();
    console.log(services);
    return (
-      <div className="service-section">
-         <div className="services">
-            {services.map((service) => (
-               <Service key={service._id} service={service}></Service>
-            ))}
+      <div>
+         <Slider></Slider>
+         <div className="service-section">
+            <div className="services">
+               {services.map((service) => (
+                  <Service key={service._id} service={service}></Service>
+               ))}
+            </div>
+            <Link to="/services">
+               <button className="btn btn-primary mt-10">See All</button>
+            </Link>
          </div>
-         <Link to='/services'><button className="btn btn-primary mt-10">See All</button></Link>
-         
       </div>
    );
 };
--- src/components/Home/Slider/Slider.js
+++ src/components/Home/Slider/Slider.js
@@ -1,11 +1,66 @@
-import React from 'react';
+import React from "react";
+import firstImg from '../../../assets/fridge-repair.jpg';
+import secondImg from '../../../assets/images (1).jpg';
+import thiredImg from '../../../assets/istockphoto-1096963798-612x612.jpg';
+
 
 const Slider = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+
+    const sliderData = [
+        {
+
+        }
+    ]
+
+   return (
+      <div className="carousel w-full">
+         <div id="slide1" className="carousel-item relative w-full">
+            <img
+               src="https://placeimg.com/800/200/arch"
+               className="w-full"
+               alt=""
+            />
+            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+               <a href="#slide4" className="btn btn-circle">
+                  ❮
+               </a>
+               <a href="#slide2" className="btn btn-circle">
+                  ❯
+               </a>
+            </div>
+         </div>
+         <div id="slide2" className="carousel-item relative w-full">
+            <img
+               src="https://placeimg.com/800/200/arch"
+               className="w-full"
+               alt=""
+            />
+            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+               <a href="#slide1" className="btn btn-circle">
+                  ❮
+               </a>
+               <a href="#slide3" className="btn btn-circle">
+                  ❯
+               </a>
+            </div>
+         </div>
+         <div id="slide3" className="carousel-item relative w-full">
+            <img
+               src="https://placeimg.com/800/200/arch"
+               className="w-full"
+               alt=""
+            />
+            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+               <a href="#slide2" className="btn btn-circle">
+                  ❮
+               </a>
+               <a href="#slide4" className="btn btn-circle">
+                  ❯
+               </a>
+            </div>
+         </div>
+      </div>
+   );
 };
 
 export default Slider;
