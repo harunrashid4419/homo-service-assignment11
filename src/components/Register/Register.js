0e837d45e6defebad53d4eb2892bb850944787f1
harunorrashid4419 <harunrashid4419@gmail.com>
2022-11-08 11:17:12 +0600

    registion complate

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,13 +1,36 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
-import './Register.css';
-import registerImg from "../../assets/7.webp";
+import { AuthContext } from "../../Context/UserContext";
+import "./Register.css";
+
 
 const Register = () => {
+    const {createUser} = useContext(AuthContext);
+
+
+    const handleSubmit = event =>{
+        event.preventDefault();
+
+        const form = event.target;
+        const name = form.name.value;
+        const photoURL = form.photoURL.value;
+        const email = form.email.value;
+        const password = form.password.value;
+        
+        createUser(email, password)
+            .then(result =>{
+                const user = result.user;
+                console.log(user)
+            })
+            .catch( err => console.error(err))
+    }
+
    return (
       <div className="registration-section">
-         <form>
-            <h1 className="text-center text-4xl text-orange-500">Please Register</h1>
+         <h1 className="text-center text-4xl text-orange-500">
+            Please Register
+         </h1>
+         <form onSubmit={handleSubmit}>
             <div className="form-control">
                <label className="label">
                   <span className="label-text">Enter Name</span>
@@ -55,7 +78,10 @@ const Register = () => {
             <div className="form-control mt-6">
                <button className="btn btn-primary">Register</button>
                <p className="text-center py-5">
-                  Already have an account <Link className="text-success" to="/login">Login</Link>{" "}
+                  Already have an account{" "}
+                  <Link className="text-success" to="/login">
+                     Login
+                  </Link>{" "}
                </p>
             </div>
          </form>
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCCBrqlct0yzYkt-4xpJNuxXFGO-d8d5KE",
+  authDomain: "ervice-ad366.firebaseapp.com",
+  projectId: "ervice-ad366",
+  storageBucket: "ervice-ad366.appspot.com",
+  messagingSenderId: "816994016856",
+  appId: "1:816994016856:web:c5aeb368206f12bd89b42d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
